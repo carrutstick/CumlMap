31366840c71e3ec952a0f08dce9d4bd6cca10797
Isaac Carruthers <icarruthers@quantitativebrokers.com>
2018-09-17 16:49:54 -0400

    Refactor the raw AA tree code a bit

--- src/arne_raw.rs
+++ src/arne_raw.rs
@@ -2,6 +2,7 @@ extern crate num_traits;
 use num_traits::Zero;
 use std::ops::{Add, Sub};
 use std::ptr;
+use std::cmp::Ordering;
 
 use cmap::*;
 
@@ -12,28 +13,29 @@ use cmap::*;
 struct AARCumlNode<K, V> {
     index: K,
     val: V,
-    left: Node<K, V>,
-    right: Node<K, V>,
+    left: NodeRef<K, V>,
+    right: NodeRef<K, V>,
     level: usize,
 }
 
-struct Node<K, V>(*mut AARCumlNode<K, V>);
+type Node<K, V> = AARCumlNode<K, V>;
+struct NodeRef<K, V>(*mut AARCumlNode<K, V>);
 
-impl<K, V> Clone for Node<K, V> {
-    fn clone(&self) -> Node<K, V> {
-        Node(self.0)
+impl<K, V> Clone for NodeRef<K, V> {
+    fn clone(&self) -> NodeRef<K, V> {
+        NodeRef(self.0)
     }
 }
 
-impl<K, V> Copy for Node<K, V> {}
+impl<K, V> Copy for NodeRef<K, V> {}
 
-impl<K, V> Node<K, V> {
-    fn null() -> Node<K, V> {
-        Node(ptr::null_mut())
+impl<K, V> NodeRef<K, V> {
+    fn null() -> NodeRef<K, V> {
+        NodeRef(ptr::null_mut())
     }
 
-    fn new(k: K, v: V) -> Node<K, V> {
-        Node(Box::into_raw(Box::new(AARCumlNode {
+    fn new(k: K, v: V) -> NodeRef<K, V> {
+        NodeRef(Box::into_raw(Box::new(AARCumlNode {
             index: k,
             val: v,
             left: Self::null(),
@@ -43,55 +45,135 @@ impl<K, V> Node<K, V> {
     }
 
     unsafe fn free(&mut self) {
-        if !self.is_null() {
-            self.left().free();
-            self.right().free();
+        if !self.0.is_null() {
+            self.borrow_mut().map(|x| x.left().free());
+            self.borrow_mut().map(|x| x.right().free());
             Box::from_raw(self.0);
         }
     }
 
-    fn is_null(&self) -> bool {
-        self.0.is_null()
-    }
-
-    unsafe fn left(&self) -> Node<K, V> {
-        (*self.0).left
-    }
-
-    unsafe fn right(&self) -> Node<K, V> {
-        (*self.0).right
-    }
-
-    unsafe fn level(&self) -> usize {
-        (*self.0).level
-    }
-
-    unsafe fn set_val(&mut self, val: V) {
-        (*self.0).val = val
-    }
-
-    unsafe fn set_left(&mut self, l: Node<K, V>) {
-        (*self.0).left = l
-    }
-
-    unsafe fn set_right(&mut self, r: Node<K, V>) {
-        (*self.0).right = r
-    }
-
-    unsafe fn incr_level(&mut self) {
-        (*self.0).level += 1
+    fn borrow_mut(&self) -> Option<&mut Node<K, V>> {
+        if self.0.is_null() {
+            None
+        } else {
+            unsafe { Some(&mut *self.0) }
+        }
     }
 }
 
-impl<K: Copy, V> Node<K, V> {
-    unsafe fn index(&self) -> K {
-        (*self.0).index
+impl<K, V> Node<K, V> {
+    fn left(&self) -> NodeRef<K, V> {
+        self.left
+    }
+
+    fn right(&self) -> NodeRef<K, V> {
+        self.right
+    }
+
+    fn level(&self) -> usize {
+        self.level
+    }
+
+    fn set_val(&mut self, val: V) {
+        self.val = val
+    }
+
+    fn set_left(&mut self, l: NodeRef<K, V>) {
+        self.left = l
+    }
+
+    fn set_right(&mut self, r: NodeRef<K, V>) {
+        self.right = r
+    }
+
+    fn incr_level(&mut self) {
+        self.level += 1
     }
 }
 
-impl<K, V: Copy> Node<K, V> {
-    unsafe fn val(&self) -> V {
-        (*self.0).val
+impl<K: Clone, V> Node<K, V> {
+    fn index(&self) -> K {
+        self.index.clone()
+    }
+}
+
+impl<K, V: Clone> Node<K, V> {
+    fn val(&self) -> V {
+        self.val.clone()
+    }
+}
+
+impl<K, V> NodeRef<K, V>
+where
+    K: Add<Output = K> + Sub<Output = K> + Zero + Copy + Ord,
+    V: Add<Output = V> + Sub<Output = V> + Zero + Copy + Ord,
+{
+    fn insert_node(np: NodeRef<K, V>, k: K, v: V) -> NodeRef<K, V> {
+        match np.borrow_mut() {
+            None => NodeRef::new(k, v),
+            Some(n) => {
+                if k < n.index() {
+                    n.set_val(n.val() + v);
+                    n.set_left(Self::insert_node(n.left(), k, v))
+                } else if k > n.index() {
+                    n.set_right(Self::insert_node(n.right(), k, v))
+                } else {
+                    n.set_val(n.val() + v);
+                    return np;
+                }
+                Self::split_node(Self::skew_node(np))
+            },
+        }
+    }
+
+    fn skew_node(np: NodeRef<K, V>) -> NodeRef<K, V> {
+        match np.borrow_mut() {
+            None => NodeRef::null(),
+            Some(n) => {
+                let lp = n.left();
+                match lp.borrow_mut() {
+                    None => np,
+                    Some(l) => {
+                        if l.level() == n.level() {
+                            n.set_left(l.right());
+                            l.set_right(np);
+                            n.set_val(n.val() - l.val());
+                            lp
+                        } else {
+                            np
+                        }
+                    },
+                }
+            },
+        }
+    }
+
+    fn split_node(np: NodeRef<K, V>) -> NodeRef<K, V> {
+        match np.borrow_mut() {
+            None => NodeRef::null(),
+            Some(n) => {
+                let rp = n.right();
+                match rp.borrow_mut() {
+                    None => np,
+                    Some(r) => {
+                        match r.right().borrow_mut() {
+                            None => np,
+                            Some(rr) => {
+                                if rr.level() == n.level() {
+                                    n.set_right(r.left());
+                                    r.set_val(r.val() + n.val());
+                                    r.set_left(np);
+                                    r.incr_level();
+                                    rp
+                                } else {
+                                    np
+                                }
+                            },
+                        }
+                    },
+                }
+            },
+        }
     }
 }
 
@@ -100,141 +182,52 @@ where
     K: Add<Output = K> + Sub<Output = K> + Zero + Copy + Ord,
     V: Add<Output = V> + Sub<Output = V> + Zero + Copy + Ord,
 {
-    fn insert_node(mut n: Node<K, V>, k: K, v: V) -> Node<K, V> {
-        if n.is_null() {
-            Node::new(k, v)
-        } else {
-            unsafe {
-                if k < n.index() {
-                    n.set_val(n.val() + v);
-                    n.set_left(Self::insert_node(n.left(), k, v))
-                } else if k > n.index() {
-                    n.set_right(Self::insert_node(n.right(), k, v))
-                } else {
-                    n.set_val(n.val() + v);
-                    return n;
-                }
-                Self::split_node(Self::skew_node(n))
-            }
-        }
-    }
-
-    fn skew_node(mut n: Node<K, V>) -> Node<K, V> {
-        if n.is_null() {
-            n
-        } else {
-            unsafe {
-                if n.left().is_null() {
-                    n
-                } else {
-                    let mut l = n.left();
-                    if l.level() == n.level() {
-                        n.set_left(l.right());
-                        l.set_right(n);
-                        n.set_val(n.val() - l.val());
-                        l
-                    } else {
-                        n
-                    }
-                }
-            }
-        }
-    }
-
-    fn split_node(mut n: Node<K, V>) -> Node<K, V> {
-        if n.is_null() {
-            n
-        } else {
-            unsafe {
-                if n.right().is_null() {
-                    n
-                } else {
-                    let mut r = n.right();
-                    if r.right().is_null() {
-                        n
-                    } else {
-                        if r.right().level() == n.level() {
-                            n.set_right(r.left());
-                            r.set_val(r.val() + n.val());
-                            r.set_left(n);
-                            r.incr_level();
-                            r
-                        } else {
-                            n
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     fn get_total(&self) -> V {
-        if self.is_null() {
-            V::zero()
-        } else {
-            unsafe { self.val() + self.right().get_total() }
+        self.val() + match self.right().borrow_mut() {
+            None => V::zero(),
+            Some(r) => r.get_total(),
         }
     }
 
     fn get_cuml(&self, k: K, acc: V) -> V {
-        if self.is_null() {
-            acc
-        } else {
-            unsafe {
-                if k < self.index() {
-                    self.left().get_cuml(k, acc)
-                } else if k > self.index() {
-                    self.right().get_cuml(k, acc + self.val())
-                } else {
-                    acc + self.val()
-                }
-            }
+        match (k.cmp(&self.index()), self.left().borrow_mut(), self.right().borrow_mut()) {
+            (Ordering::Less, Some(l), _) => l.get_cuml(k, acc),
+            (Ordering::Less, _, _) => acc,
+            (Ordering::Greater, _, Some(r)) => r.get_cuml(k, acc + self.val()),
+            (_, _, _) => acc + self.val(),
         }
     }
 
     fn get_single(&self, k: K) -> V {
-        if self.is_null() {
-            V::zero()
-        } else {
-            unsafe {
-                if k < self.index() {
-                    self.left().get_single(k)
-                } else if k > self.index() {
-                    self.right().get_single(k)
-                } else {
-                    self.val() - self.left().get_total()
-                }
-            }
+        match (k.cmp(&self.index()), self.left().borrow_mut(), self.right().borrow_mut()) {
+            (Ordering::Equal, Some(l), _) => self.val() - l.get_total(),
+            (Ordering::Equal, _, _) => self.val(),
+            (Ordering::Less, Some(l), _) => l.get_single(k),
+            (Ordering::Greater, _, Some(r)) => r.get_single(k),
+            (_, _, _) => V::zero(),
         }
     }
 
     fn get_quantile(&self, v: V) -> Option<K> {
-        if self.is_null() {
-            None
-        } else {
-            unsafe {
-                if v > self.val() {
-                    self.right().get_quantile(v - self.val())
-                } else if v < self.val() {
-                    match self.left().get_quantile(v) {
-                        None => Some(self.index()),
-                        s => s,
-                    }
-                } else {
-                    Some(self.index())
-                }
-            }
+        match (v.cmp(&self.val()), self.left().borrow_mut(), self.right().borrow_mut()) {
+            (Ordering::Less, Some(l), _) => match l.get_quantile(v) {
+                None => Some(self.index()),
+                s => s,
+            },
+            (Ordering::Greater, _, Some(r)) => r.get_quantile(v - self.val()),
+            (Ordering::Greater, _, None) => None,
+            (_, _, _) => Some(self.index()),
         }
     }
 }
 
 pub struct AARCumlTree<K, V> {
-    root: Node<K, V>,
+    root: NodeRef<K, V>,
 }
 
 impl<K, V> AARCumlTree<K, V> {
     pub fn new() -> Self {
-        AARCumlTree { root: Node::null() }
+        AARCumlTree { root: NodeRef::null() }
     }
 }
 
@@ -253,18 +246,27 @@ where
     type Value = V;
 
     fn insert(&mut self, k: Self::Key, v: Self::Value) {
-        self.root = Node::insert_node(self.root, k, v);
+        self.root = NodeRef::insert_node(self.root, k, v);
     }
 
     fn get_cuml(&self, k: Self::Key) -> Self::Value {
-        self.root.get_cuml(k, Self::Value::zero())
+        match self.root.borrow_mut() {
+            None => V::zero(),
+            Some(r) => r.get_cuml(k, V::zero()),
+        }
     }
 
     fn get_single(&self, k: Self::Key) -> Self::Value {
-        self.root.get_single(k)
+        match self.root.borrow_mut() {
+            None => V::zero(),
+            Some(r) => r.get_single(k),
+        }
     }
 
     fn get_quantile(&self, quant: Self::Value) -> Option<Self::Key> {
-        self.root.get_quantile(quant)
+        match self.root.borrow_mut() {
+            None => None,
+            Some(r) => r.get_quantile(quant),
+        }
     }
 }
